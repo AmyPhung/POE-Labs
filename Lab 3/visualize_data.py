a7a647a2baaff1f7a3681aa801fdf5432a7ab891
AmyPhung <amyngph@gmail.com>
2019-10-11 15:07:20 -0400

    Remove extra comments

--- Lab 3/visualize_data.py
+++ Lab 3/visualize_data.py
@@ -32,30 +32,4 @@ ax3.set_ylabel("Speed PWM Command \n (between 0 and 255)")
 
 plt.xlabel("Time (s)")
 
-
-
-
-#
-# plt.subplot(3, 1, 2)
-# plt.plot(data["Time"], data["NLSensor"])
-#
-# plt.subplot(3, 1, 3)
-# plt.plot(data["Time"], data["NLSensor"])
-
 plt.show()
-#
-# f = open("raw_data/raw_data.txt", "r")
-# data_str = f.read()
-# # data_list = data_str.split("\n")
-# # data_list2 = [i[15:] for i in data_list]
-#
-# from io import StringIO
-# import csv
-#
-# f2 = StringIO(data_str)
-# reader = csv.reader(f, delimiter = ',')
-# # for row in reader:
-# #     print('\t'.join(row))
-#
-# for row in reader:
-#     print(row)
